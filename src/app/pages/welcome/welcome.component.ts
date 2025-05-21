8032ab27dc2cea630e3bbb71a0ca27c5d594eb5d
OkayLetsG <Ghaleb.steam1@gmail.com>
2025-05-21 13:44:45 -0700

    added a demo example for folders

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FoldersComponent } from './folders/folders.component';
+import { EditorComponent } from './editor/editor.component';
+import { SnippetsComponent } from './snippets/snippets.component';
+import { NzSplitterModule } from 'ng-zorro-antd/splitter';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
+  imports: [FoldersComponent, EditorComponent, SnippetsComponent, NzSplitterModule]
 })
 export class WelcomeComponent {
-  constructor() {}
+
 }
